528f01799020f0562c5c74c346a54bdf241f1267
charlottemorabito <cmorabito@gmail.com>
2015-10-04 17:54:25 -0400

    Update first.js

--- first.js
+++ first.js
@@ -1,1 +1,24 @@
+<script>window.twttr = (function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0],
+    t = window.twttr || {};
+  if (d.getElementById(id)) return t;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://platform.twitter.com/widgets.js";
+  fjs.parentNode.insertBefore(js, fjs);
+ 
+  t._e = [];
+  t.ready = function(f) {
+    t._e.push(f);
+  };
+ 
+  return t;
+}(document, "script", "twitter-wjs"));</script>
 
+<script src="https://api.twitter.com/1.1/users/lookup.json"></script>
+
+
+<script type="text/javascript">
+
+
+</script>
