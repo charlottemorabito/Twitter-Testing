40919fbec4136e2de0501eb8b5c042c7666c1357
charlottemorabito <cmorabito@gmail.com>
2015-10-04 17:44:17 -0400

    Update and rename first.php to first.js

--- first.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once('/var/www/html/sign_up/twitteroauth/twitteroauth.php');
-require_once('/var/www/html/sign_up/config.php');
-require_once('/var/www/apbb/util.php');
-
-$following = friends_count;
